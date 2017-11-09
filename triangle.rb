5e698dcd90a14b6c6fcca69b606f190ddcd3a685
Annie Pennell <anniecpennell@gmail.com>
2017-11-09 11:09:13 -0800

    previous solution just hard-coded theanswer they wanted; here I put the actual checks for valid triangles in the triangle method and raised errors if invalid; also refactored triangle type-check.

--- triangle.rb
+++ triangle.rb
@@ -14,12 +14,21 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  # sort so first is definitely lowest number, last is highest
+  x, y, z = [a, b, c].sort
+
+  # all sides must be > 0
+  # only need to check first side because if it's not a problem in the lowest one, won't be on the others either
+  raise TriangleError.new("All sides of a triangle must be greater than 0.") if x <= 0
+
+  # the sum of any 2 sides must be greater than the 3rd
+  # only need to check sum of 2 smallest because that's where an error would occur first
+  raise TriangleError.new("The sum of any two sides of a triangle must be greater than the third.") if x + y <= z
+  
+  # if no errors raised, we have a valid triangle and must determine what type
   if a == b && b == c
   	return :equilateral
-  elsif (a == b && a != c) ||
-  			(a != b && a == c) ||
-  			(b == c && b != a)
+  elsif a == b || a == c || b == c
   	return :isosceles
   else
   	return :scalene
@@ -29,3 +38,4 @@ end
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
 end
+
